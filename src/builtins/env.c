dbefa2abdefa270096a4cb5f8b1c1a0f9b452964
Saliinger <alexisnoukan@gmail.com>
2024-08-12 16:42:13 +0200

    start of env function

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -1,1 +1,14 @@
 // display env variable
+
+#include "../../include/minishell.h"
+
+void    ft_env(t_minishell *minishell)
+{
+    int i = 0;
+
+    while (minishell->env[i])
+    {
+        printf("%s\n", minishell->env[i]);
+        i++;
+    }
+}
