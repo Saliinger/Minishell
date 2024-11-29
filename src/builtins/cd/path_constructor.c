2756bf70df5800fa20987cf5bed3bbb7b93d9b8c
Saliinger <alexisnoukan@gmail.com>
2024-11-29 10:27:13 +0100

    update cd + need fix segfault

--- src/builtins/cd/path_constructor.c
+++ src/builtins/cd/path_constructor.c
@@ -6,77 +6,92 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 12:37:06 by anoukan           #+#    #+#             */
-/*   Updated: 2024/11/13 14:36:01 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/11/18 15:36:46 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
 
-static void	remove_path(char *dest, char *path)
+static void	remove_path(char *dest)
 {
-	int	len;
+    int len;
 
-	len = ft_strlen(path);
-	while (path[len] != '/')
-		len--;
-	ft_strlcpy(dest, path, len);
+    len = ft_strlen(dest);
+    while (dest[len] != '/')
+    {
+        dest[len] = '\0';
+        len--;
+    }
 }
 
-static void	add_path(char *dest, char *path, char *to_add, int *i)
+static void	add_path(char *dest, char *to_add)
 {
-	int	j;
-	int	len;
+    int	j;
+    int i;
+    int	len;
 
-	j = *i;
-	while (to_add[j] && to_add[j] != '/')
-		j++;
-	len = ft_strlen(path);
+    len = ft_strlen(dest);
+    dest[len] = '/';
+    j = len + 1;
+    i = 0;
+    while (to_add[i])
+    {
+        dest[j] = to_add[i];
+        j++;
+        i++;
+    }
+    dest[j] = '\0';
 }
 
 static void	init_path(char *current_path, char **in_cut, t_minishell *minishell,
-		int *i)
+                         int *i)
 {
-	char	*temp;
+    char	*temp;
 
-	if (*in_cut[0] == '~')
-	{
-		temp = get_home(minishell);
-		ft_strlcpy(current_path, temp, ft_strlen(temp));
-		free(temp);
-		*i = 1;
-	}
-	else
-	{
-		temp = get_current_path(minishell);
-		ft_strlcpy(current_path, temp, ft_strlen(temp));
-		free(temp);
-		*i = 0;
-	}
+    if (*in_cut[0] == '~')
+    {
+        temp = get_home(minishell);
+        ft_strlcpy(current_path, temp, ft_strlen(temp));
+        free(temp);
+        *i = 1;
+    }
+    else
+    {
+        temp = get_current_path(minishell);
+        ft_strlcpy(current_path, temp, ft_strlen(temp));
+        free(temp);
+        *i = 0;
+    }
 }
 
 char	*path_constructor(t_minishell *minishell, char *in)
 {
-	char	*res;
-	char	current_path[PATH_MAX];
-	int		i;
-	char	**in_cut;
+    char	*res;
+    char	current_path[PATH_MAX];
+    int		i;
+    int     j;
+    char	**in_cut;
 
-	in_cut = ft_split(in, '/');
-	init_path(current_path, in_cut, minishell, &i);
-	while (in_cut[i])
-	{
-		if (ft_strlen(in_cut[i]) <= 3)
-		{
-		}
-		else
-		{
-			ft_strjoin(current_path, "/");
-			ft_strjoin(current_path, in[i]);
-			ft_strjoin(current_path, "/");
-		}
-		i++;
-	}
-	return (res);
+    in_cut = ft_split(in, '/');
+    init_path(current_path, in_cut, minishell, &i);
+    while (in_cut[i])
+    {
+        j = 0;
+        if (ft_strlen(in_cut[i]) <= 3)
+        {
+            while (in_cut[i][j] && in_cut[i][j] == '.')
+                j++;
+            if (j == 2)
+                remove_path(current_path);
+            else if (j > 2)
+                add_path(current_path, in_cut[i]);
+        }
+        else
+            add_path(current_path, in_cut[i]);
+        i++;
+    }
+    res = ft_strdup(current_path);
+    return (res);
 }
 
 // ../
