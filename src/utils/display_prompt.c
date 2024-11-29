8181beb5f7d52780a02361f8897a3b12c4dd7963
Saliinger <alexisnoukan@gmail.com>
2024-11-29 15:38:21 +0100

    fix prompt

--- src/utils/display_prompt.c
+++ src/utils/display_prompt.c
@@ -12,6 +12,29 @@
 
 #include "../../include/minishell.h"
 
+static char *get_last_path(char *path)
+{
+    char *res;
+    int i;
+    int j;
+
+    i = ft_strlen(path);
+    while (path[i] != '/')
+        i--;
+    res = (char *)malloc(sizeof(char) * (ft_strlen(path) - i + 1));
+    if (!res)
+        return (NULL);
+    j = 0;
+    while (path[i])
+    {
+        res[j] = path[i];
+        j++;
+        i++;
+    }
+    res[j] = '\0';
+    return (res);
+}
+
 char	*display_prompt(char *prompt, t_minishell *minishell)
 {
 	char	*path;
@@ -20,7 +43,7 @@ char	*display_prompt(char *prompt, t_minishell *minishell)
 
 	if (prompt)
 		free(prompt);
-	path = minishell->pwd;
+	path = get_last_path(minishell->pwd);
 	usr = getenv("LOGNAME");
 	res = ft_strjoin(AINSI_BGREEN, usr);
 	res = ft_strjoin(res, "@");
@@ -32,5 +55,6 @@ char	*display_prompt(char *prompt, t_minishell *minishell)
 	res = ft_strjoin_frees1(res, path); //fixme parcours str, et si /home/$USER -> replace by ~
 	res = ft_strjoin_frees1(res, AINSI_RESET);
 	res = ft_strjoin_frees1(res, " ");
+    free(path);
 	return (res);
 }
