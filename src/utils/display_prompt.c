8acd25a1c6cb8065357d966b02627479de628392
ekrebs <ekrebs.contact@gmail.com>
2024-11-29 05:37:30 +0100

    added comments in display_prompt.c for prompt fix. -- tommorow, will be working on bringing heredocs to life.

--- src/utils/display_prompt.c
+++ src/utils/display_prompt.c
@@ -6,7 +6,7 @@
 /*   By: ekrebs <ekrebs@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/27 12:34:56 by anoukan           #+#    #+#             */
-/*   Updated: 2024/11/29 05:22:18 by ekrebs           ###   ########.fr       */
+/*   Updated: 2024/11/29 05:35:34 by ekrebs           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,12 +24,12 @@ char	*display_prompt(char *prompt, t_minishell *minishell)
 	usr = getenv("LOGNAME");
 	res = ft_strjoin(AINSI_BGREEN, usr);
 	res = ft_strjoin(res, "@");
-	res = ft_strjoin_frees1(res, "Femboys"); //fixme
+	res = ft_strjoin_frees1(res, "Femboys"); //fixme : env NAME=
 	res = ft_strjoin_frees1(res, AINSI_RESET);
 	res = ft_strjoin_frees1(res, ":");
 	res = ft_strjoin_frees1(res, AINSI_BLUE);
 	res = ft_strjoin_frees1(res, "~");
-	res = ft_strjoin_frees1(res, path);
+	res = ft_strjoin_frees1(res, path); //fixme parcours str, et si /home/$USER -> replace by ~
 	res = ft_strjoin_frees1(res, AINSI_RESET);
 	res = ft_strjoin_frees1(res, " ");
 	return (res);
