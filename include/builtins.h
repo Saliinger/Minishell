817179b2babd9201befc864106fcf943f29ce904
Saliinger <alexisnoukan@gmail.com>
2024-12-10 15:32:37 +0100

    cd norm done

--- include/builtins.h
+++ include/builtins.h
@@ -3,21 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ekrebs <ekrebs@student.42.fr>              +#+  +:+       +#+        */
+/*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 23:06:59 by anoukan           #+#    #+#             */
-/*   Updated: 2024/11/28 23:42:31 by ekrebs           ###   ########.fr       */
+/*   Updated: 2024/12/10 15:32:05 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUILTINS_H
 # define BUILTINS_H
 
-# include <stdio.h>
-# include <readline/readline.h>
-# include <readline/history.h>
 # include "imports.h"
 # include "structure.h"
+# include <readline/history.h>
+# include <readline/readline.h>
+# include <stdio.h>
 # include <sys/stat.h>
 
 // CD
@@ -28,11 +28,13 @@ char	*get_home(t_minishell *minishell);
 char	*path_constructor(t_minishell *minishell, char *in);
 char	*get_current_path(t_minishell *minishell);
 int		change_pwd(t_minishell *minishell, char *in);
+void	remove_path(char *dest);
+void	add_path(char *dest, char *to_add);
 
 // ECHO
 int		ft_echo(t_command_exec *command);
-void    echo_print(char **arg, bool eof, int i);
-int     check_flag(char **arg);
+void	echo_print(char **arg, bool eof, int i);
+int		check_flag(char **arg);
 
 // ENV
 int		ft_env(t_minishell *minishell);
@@ -41,21 +43,21 @@ int		ft_env(t_minishell *minishell);
 int		ft_exit(t_minishell *minishell, t_command_exec *command, bool fail);
 
 // EXPAND
-int     ft_expand(t_command_exec *command, t_minishell *minishell);
+int		ft_expand(t_command_exec *command, t_minishell *minishell);
 
 // PWD
 int		ft_pwd(t_minishell *minishell);
 
 // UNSET
-int     ft_unset(t_command_exec *command, t_minishell *minishell);
-int     count_var(char **var);
-char    **new_env(t_minishell *minishell, int *to_remove, int nbr_of_line);
+int		ft_unset(t_command_exec *command, t_minishell *minishell);
+int		count_var(char **var);
+char	**new_env(t_minishell *minishell, int *to_remove, int nbr_of_line);
 
 // EXPORT
 int		ft_export(t_command_exec *command, t_minishell *minishell);
-int     create_var(t_minishell *minishell, char *var);
-int     delete_var(t_minishell *minishell, int line);
-bool check_var_equals(char *in);
-bool check_var_name(char *in);
+int		create_var(t_minishell *minishell, char *var);
+int		delete_var(t_minishell *minishell, int line);
+bool	check_var_equals(char *in);
+bool	check_var_name(char *in);
 
 #endif
--- src/builtins/cd/path_constructor.c
+++ src/builtins/cd/path_constructor.c
@@ -6,68 +6,31 @@
 /*   By: anoukan <anoukan@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/08 12:37:06 by anoukan           #+#    #+#             */
-/*   Updated: 2024/12/10 12:51:47 by anoukan          ###   ########.fr       */
+/*   Updated: 2024/12/10 15:32:09 by anoukan          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../../include/minishell.h"
-
-static void	remove_path(char *dest)
-{
-	int	len;
-
-	len = ft_strlen(dest);
-	if (len == 0)
-		return ;
-	while (len > 0 && dest[len - 1] != '/')
-		len--;
-	if (len == 0)
-		dest[0] = '\0';
-	else
-		dest[len - 1] = '\0';
-}
-
-static void	add_path(char *dest, char *to_add)
-{
-	int	j;
-	int	i;
-	int	len;
-
-	len = ft_strlen(dest);
-	dest[len] = '/';
-	j = len + 1;
-	i = 0;
-	while (to_add[i])
-	{
-		dest[j] = to_add[i];
-		j++;
-		i++;
-	}
-	dest[j] = '\0';
-}
 
 // init path handle the start of the path that we wan't to go to ~ / or nothing
 static int	init_path(char *in, t_minishell *minishell, char **new_in)
 {
 	char	*temp;
 
-	if (!in)
-		return (0);
-	if (*in == '~' || *in == '/')
+	if (*in == '~' || *in == '/' || !in)
 	{
 		temp = get_home(minishell);
-		if (*in != '~' && ft_strncmp(temp, in, ft_strlen(temp)) != 0)
+		if (*in != '~' && ft_strcmp(temp, in) != 0)
 			*new_in = ft_strjoin(temp, in);
-		else if (ft_strncmp(temp, in, ft_strlen(temp)) != 0)
+		else if (ft_strcmp(temp, in) != 0)
 			*new_in = ft_strjoin(temp, in + 1);
 		else
 			*new_in = ft_strdup(in);
-		free(temp);
 	}
 	else
 	{
 		temp = get_current_path(minishell);
-		if (ft_strncmp(temp, in, ft_strlen(temp)) != 0)
+		if (ft_strcmp(temp, in) != 0)
 		{
 			if (*in != '/')
 				temp = ft_strjoin_frees1(temp, "/");
@@ -75,9 +38,8 @@ static int	init_path(char *in, t_minishell *minishell, char **new_in)
 		}
 		else
 			*new_in = ft_strdup(in);
-		free(temp);
 	}
-	return (0);
+	return (free(temp), 0);
 }
 
 bool	check_dot(char *in)
@@ -100,39 +62,51 @@ bool	check_dot(char *in)
 	return (nbr);
 }
 
+int	nbr_of_dot(char *line)
+{
+	int	nbr;
+
+	nbr = 0;
+	while (line[nbr] == '.')
+		nbr++;
+	return (nbr);
+}
+
+char	*get_res(char *new_in, char **in_cut, char *current_path)
+{
+	char	*res;
+
+	res = ft_strdup(current_path);
+	ft_free_tab(in_cut);
+	free(new_in);
+}
+
 char	*path_constructor(t_minishell *minishell, char *in)
 {
 	char	*res;
 	char	*new_in;
 	char	current_path[PATH_MAX];
 	int		i;
-	int		j;
 	char	**in_cut;
 
-	new_in = NULL;
 	i = init_path(in, minishell, &new_in);
 	if (!check_dot(new_in))
 		return (new_in);
 	in_cut = ft_split(new_in, '/');
 	while (in_cut[i])
 	{
-		j = 0;
-		if (ft_strlen(in_cut[i]) <= 3 && in_cut[i][j] == '.')
+		if (ft_strlen(in_cut[i]) <= 3 && *in_cut[i] == '.')
 		{
-			while (in_cut[i][j] && in_cut[i][j] == '.')
-				j++;
-			if (j == 2)
+			if (nbr_of_dot(in_cut[i]) == 2)
 				remove_path(current_path);
-			else if (j > 2)
+			else if (nbr_of_dot(in_cut[i]) > 2)
 				add_path(current_path, in_cut[i]);
 		}
 		else
 			add_path(current_path, in_cut[i]);
 		i++;
 	}
-	res = ft_strdup(current_path);
-	ft_free_tab(in_cut);
-	free(new_in);
+	res = get_res(new_in, in_cut, current_path);
 	return (res);
 }
 
